6a08c23925f2418b0716b83e430df49b0a74826d
Iluxa727 <114696356+Iluxa727@users.noreply.github.com>
2022-12-16 11:10:23 +0200

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -559,8 +559,10 @@ class Hikka:
 
             logo1 = f"""
 
-                        █ █ █ █▄▀ █▄▀ ▄▀█
-                        █▀█ █ █ █ █ █ █▀█
+                  
+                         █▀▄▀█ ▀     █ █   █▀▀█  █▀▀▀█ █▀▀▀█ 
+                         █ ▀ █  ▀█▀   █▀▄   █  █  ▀▀▀▄▄  ▀▀▀▄▄ 
+                         ▀   ▀  ▀▀▀   ▀ ▀   ▀▀▀▀  █▄▄▄█  █▄▄▄█
 
                      • Build: {build[:7]}
                      • Version: {'.'.join(list(map(str, list(__version__))))}
@@ -576,7 +578,7 @@ class Hikka:
                     else ""
                 )
                 logging.info(
-                    "🌘 Hikka %s started\n🔏 GitHub commit SHA: %s (%s)\n%s%s",
+                    "🌘 Mikoss %s started\n🔏 GitHub commit SHA: %s (%s)\n%s%s",
                     ".".join(list(map(str, list(__version__)))),
                     build[:7],
                     upd,
